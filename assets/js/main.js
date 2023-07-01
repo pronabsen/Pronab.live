acb868d736cc7754e4dded27b3312bd03915b374
pronabsen <pronabsen18@gmail.com>
2023-07-01 15:52:02 +0600

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,99 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".nextzenlimited",
+  `Write modern, performant, maintainable code for a diverse 
+  array of client and internal projects Work with a variety of different l
+  anguages, platforms, frameworks, and content management systems such 
+  as Java, Php, Laravel, WordPress, Python`,
+  "Software Engineer",
+  "Nextzen Limited",
+  "September 2018 - Present *"
+);
+
+hoverChangeExperience(
+  ".bdtask",
+  `Design and Build sophisticated and highly salable apps using Flutter. 
+  Translate and Build the designs into high-quality responsive UI code. 
+  Solid knowledge of Dart programming language. Resolve any problems 
+  existing in the system and suggest and add new features in the complete system.`,
+  "Internship as Flutter Developer",
+  "BDTask",
+  "Oct 2022 - Jan 2023 (4 Month)"
+);
+
+hoverChangeExperience(
+  ".spidysoft",
+  `Developed and shipped highly interactive Android applications 
+  for SpidySoft IT. Built many types of Apps like News, Videos, Hr etc.`,
+  "Social Media e Designer",
+  "Jr. Android Developer",
+  "May 2017 - April 2019 (2 Year)"
+);
+
+hoverChangeExperience(
+  ".contweb",
+  `Realizei o estágio na ContWeb, uma empresa de contabilidade. 
+  Onde pela primeira vez tive experiência com o React. Além do
+  desenvolvimento frontend criei os designs da nova plataforma da empresa.`,
+  "Developer frontend e UI Designer",
+  "ContWeb",
+  "Set 2021 - Nov 2021 (3 meses)"
+);
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
+);
+hoverChangeDescription(
+  ".react",
+  "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
+);
+hoverChangeDescription(
+  ".styled",
+  "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
+);
+hoverChangeDescription(
+  ".radix",
+  "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
+);
+hoverChangeDescription(
+  ".cypress",
+  "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
+);
+hoverChangeDescription(
+  ".storybook",
+  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+);
